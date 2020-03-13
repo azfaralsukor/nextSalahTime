32bb40e8d49525d205df81a05cb1a96159b92494
gigantor <azfaralsukor@gmail.com>
2020-03-14 02:13:05 +0800

    init deployment

--- README.md
+++ README.md
@@ -1,1 +1,58 @@
-# nextSalahTime
+# Next Salah Time
+
+Gets the next salah time with remaining time before next salah in Malaysia. A work in progress.
+
+## Getting Started
+
+These instructions will get you a copy of the project up and running on your local machine for development. See deployment for notes on how to deploy the project on a live system.
+
+### File structure
+In this repository, you will find
+```
+.
+|____LICENSE
+|____server.js
+|____README.md
+|____package.json
+```
+### Prerequisites
+
+You should have these installed on your machine before getting started
+* [git](https://git-scm.com/)
+* [NodeJS](https://nodejs.org/en/)
+* [MongoDB](https://www.mongodb.com/)
+* [Visual Studio Code](https://code.visualstudio.com/)_(optional)_ 
+
+### Clone / Download
+
+You may download ZIP file and extract the files, clone with [GitHub Desktop](https://desktop.github.com/), or
+
+```
+cd /your-desired-path
+git clone https://github.com/azfaralsukor/nextSalahTime.git
+```
+
+### Installing
+
+```
+cd nextSalahTime
+yarn install
+npm start
+```
+
+There you have it.
+
+## Deployment
+
+Login or [create](https://signup.heroku.com/) a heroku account and press the button below to host as your own.
+
+[![Deploy](https://www.herokucdn.com/deploy/button.png)](https://heroku.com/deploy)
+
+## Built With
+
+* [NodeJS](https://nodejs.org/en/)
+* [ExpressJS](https://expressjs.com/) - The framework
+
+## License
+
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ server.js
@@ -0,0 +1,67 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cors());
+
+const now = new Date(new Date('2020-03-14T18:25:00Z'))//.getTime() + 480 * 60000);
+const ISOizeDate = date => new Date(`${now.toISOString().substring(0, now.toISOString().indexOf('T'))}T${date}Z`);
+const capitalizeFirstLetter = (string = "") => string.charAt(0).toUpperCase() + string.slice(1);
+const timeLeft = (next, current) => {
+    let hourDiff = next.getHours() - current.getHours();
+    let minDiff = next.getMinutes() - current.getMinutes();
+    if (minDiff < 0) {
+        minDiff = 60 + minDiff;
+        hourDiff--;
+    }
+    return { hourDiff, minDiff };
+}
+
+const constructMsg = (next, now, name, time) => {
+    const { hourDiff, minDiff } = timeLeft(next, now);
+    if (!hourDiff && !minDiff) {
+        return `It's ${capitalizeFirstLetter(name)} (${time.substring(0, 5)}) now.`;
+    }
+    const hourStr = `${hourDiff > 0 ? `${hourDiff} hour${hourDiff > 1 ? "s" : ""}` : ""}`;
+    const minStr = `${minDiff > 0 ? `${minDiff} minute${minDiff > 1 ? "s" : ""}` : ""}`;
+    const left = hourStr && minStr ? `${hourStr} and ${minStr}` : hourStr ? hourStr : minStr;
+    return `${left} left to ${capitalizeFirstLetter(name)} (${time.substring(0, 5)})`;
+}
+
+app.get('/:zone', async (req, res) => {
+    const { zone = "WLY01" } = req.params;
+    const prayerTimes = ["fajr", "syuruk", "dhuhr", "asr", "maghrib", "isha"];
+    const url = "https://www.e-solat.gov.my/index.php";
+    let params = { r: "esolatApi/TakwimSolat", period: "today", zone };
+    let i = 0;
+    await axios.get(url, { params })
+        .then(function (response) {
+            const { prayerTime } = response.data;
+            let prayerTimeISO;
+            while (i < prayerTimes.length) {
+                prayerTimeISO = ISOizeDate(prayerTime[0][prayerTimes[i]]);
+                if (prayerTimeISO >= now) {
+                    break;
+                }
+                i++;
+            }
+            if (i < prayerTimes.length) {
+                res.send(constructMsg(prayerTimeISO, now, prayerTimes[i], prayerTime[0][prayerTimes[i]]));
+            }
+        })
+
+    if (i >= prayerTimes.length) {
+        const tommorrow = new Date(now.getTime() + 24 * 60 * 60 * 1000).toISOString();
+        params = { ...params, period: "date", date: tommorrow.substring(0, tommorrow.indexOf('T')) }
+        axios.get(url, { params }).then(function (response) {
+            res.send(constructMsg(ISOizeDate(response.data.prayerTime[0][prayerTimes[0]]), now, prayerTimes[0], response.data.prayerTime[0][prayerTimes[0]]));
+        });
+    }
+});
+
+app.listen(port, () => console.log(`Server listening on port: ${port}`));
